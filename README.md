8a6d092c310e27b9bf1ed700549cba4b13342618
willadam <willadamin@gmail.com>
2018-09-21 02:51:20 +0800

    init

--- README.md
+++ README.md
@@ -7,4 +7,10 @@
   create-react-app electrondemo
   cd electrondemo
   // 支持动态antd加载
-  
+  ```
+* 不暴露封装的配置文件来 配置项目
+  : creact-react-app 项目本质上是一个隐藏了配置文件的webpack项目，配置文件都在 react-script 这个包中
+  * reject 解压配置包，暴露所有的配置文件
+  * 一些rewired插件可以在不暴露配置文件的同时支持一些webpack配置
+  * 实现动态加载antd组件， 并且配置主题
+      : antd通过less的全局变量覆盖来配置主题， 所以要支持less
--- electrondemo/config-overrides.js
+++ electrondemo/config-overrides.js
@@ -1,4 +1,14 @@
-module.exports = function overrides(config, env){
-  // do stuff with some webpack config .....
+const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
+
+module.exports = function override(config, env){
+  config = injectBabelPlugin(
+    ['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}],
+    config
+  )
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#1DA57A"},
+    javascriptEnabled: true
+  })(config, env)
   return config
-}
+} 
