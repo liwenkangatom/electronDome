3271dc38d491e0a15f745d6cabd5a89fc891c828
willadam <willadamin@gmail.com>
2019-05-13 21:53:24 +0800

    restart projecct

--- electrondemo/src/utils/UUID.js
+++
@@ -1,6 +0,0 @@
-export default function uuid() {
-
-  // (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
-  return (new Date().getTime()|(Math.random()+1)*0x10000).toString().substring(1)
-
-}
